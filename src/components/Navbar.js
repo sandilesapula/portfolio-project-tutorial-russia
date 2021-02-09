bb991f030177a1d5f0b0046b3c28a6ee926d15ca
sandilesapula <davidsandilesapula@gmail.com>
2021-02-09 15:55:51 +0200

    Header Complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "../logo.png";
-// REACT FONTAWESOME IMPORTS
+// REACT FORTAWESOME IMPORTS
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
